a05dddf4ebc246ef59836f913bb8a0996305a3b5
NahinKh <nahinkhan77@gmail.com>
2026-07-21 15:15:29 +0600

    Implement of utils and middleware folder for error handeling and sendResponse

---
+++ src/middleware/globalErrorHandler.ts
@@ -0,0 +1,15 @@
+import { ErrorRequestHandler } from "express";
+
+const globalErrorHandler:ErrorRequestHandler=(err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+        success: false,
+        statusCode: statusCode,
+        message: message,
+        error: err.stack || "No stack trace available"
+
+    });
+}
+
+export default globalErrorHandler;
